705b88ba345fc92cea1d6fe9b51491ec2bc073f7
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2025-04-01 19:12:50 +0300

    modified configs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true, // Ensures proper file structure for static hosting
+  output: "export", // Important for static export
   images: {
     domains: ["links.papareact.com", "fakestoreapi.com"],
   },
 };
+
+module.exports = nextConfig;
